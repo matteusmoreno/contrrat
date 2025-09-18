627107f384a3cb8c1d5b771ed6bd03b6aab9e49b
Matteus Moreno <matteus_moreno@live.com>
2025-09-18 11:38:12 -0300

    first commit

---
+++ src/main/java/br/com/matteusmoreno/contrrat/ContrratApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.contrrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContrratApplication.class, args);
+	}
+
+}
